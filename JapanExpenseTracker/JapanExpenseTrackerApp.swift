4302d1fb2cf92677e9a6a4116477b383692e15bc
Aymeric-Dcn <aymeric.duchene@outlook.fr>
2026-03-15 13:39:42 +0100

    Initial Commit

---
+++ JapanExpenseTracker/JapanExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JapanExpenseTrackerApp.swift
+//  JapanExpenseTracker
+//
+//  Created by Aymeric Duchene on 15/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct JapanExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
